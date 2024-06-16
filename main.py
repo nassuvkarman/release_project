35771293e82067f23fe44d805af47a7672b7b585
marian <sadg@dsg.com>
2024-06-16 13:01:50 +0300

    gerjgiowhjguehoure

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame
 
 
@@ -8,7 +9,7 @@ FPS = 60
 
 window = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock()
-background = pygame.transform.scale(pygame.image.load("test_image.png"),SIZE)
+background = pygame.transform.scale(pygame.image.load("background_image.png"),SIZE)
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, filename, size, coords):
@@ -19,21 +20,39 @@ class GameSprite(pygame.sprite.Sprite):
         window.blit(self.image, self.rect)
 
 class Player(GameSprite):
-    def update(self):
-            keys = pygame.key.get_pressed()
+    def update_l(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a]:
+            self.rect.x -= 10
+        elif key[pygame.K_d]:
+            self.rect.x += 10
+    def update_r(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            self.rect.x -= 10
+        elif key[pygame.K_RIGHT]:
+            self.rect.x += 10
 
-            if keys[pygame.K_w]:
-                 
-                 self.rect.y -= 5
+game_over = False
+finish = False
 
-test_object = GameSprite("test_image.png", (100,100), (100,100))
-
+#test_object = GameSprite("test_image.png", (100,100), (100,100))
+car1 = Player("car_image1.png", (100,200),(WIDTH-400,HEIGH-50))
+car2 = Player("car_image2.png",(160,150), (WIDTH-600, HEIGH-50))
 game_over = False
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
-    window.blit(background, (0,0))
-    test_object.reset()
+    if not finish:
+        window.blit(background, (0,0))
+        car1.reset()
+        car1.update_r()
+        car2.reset()
+        car2.update_l()
+
+        if car1.rect.colliderect(car2.rect):
+            game_over = True
+
     pygame.display.update()
     clock.tick(FPS)
